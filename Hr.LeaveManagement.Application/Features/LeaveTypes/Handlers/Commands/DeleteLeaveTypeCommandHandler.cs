6e0e73996ee037cc31fd09272f4dcc85bc064822
Ewelike Emeka Nelson <emekaewelike@gmail.com>
2023-04-05 12:26:49 +0100

    staged

---
+++ Hr.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Hr.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hr.LeaveManagement.Application.Persistence.Contracts;
+using Hr.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+
+namespace Hr.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeRequest, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypeRequest request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.leaveTypeDto.Id);
+            if(leaveType != null)
+                await _leaveTypeRepository.Delete(leaveType);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Hr.LeaveManagement.Application/Features/LeaveTypes/Requests/Commands/DeleteLeaveTypeRequest.cs
@@ -0,0 +1,15 @@
+﻿using Hr.LeaveManagement.Application.DTOs.LeaveType;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands
+{
+    public class DeleteLeaveTypeRequest : IRequest<Unit>
+    {
+        public LeaveTypeDto leaveTypeDto { get; set; }
+    }
+}
